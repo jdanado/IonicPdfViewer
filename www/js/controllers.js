59ab16f6f4996ac6f460d800eeaa53008e9d9b35
Jose Danado <jose.danado@gmail.com>
2016-08-18 21:19:07 -0700

    PDF viewer

---
+++ www/js/controllers.js
@@ -0,0 +1,31 @@
+﻿angular.module('starter.controllers', [])
+
+.controller('PdfCtrl', function ($scope, $state) {
+    var scope = $scope;
+    var tCtrl = this;
+
+    this.onLoad = function (pag) {
+        console.log('Num pages: ' + pag);
+    };
+
+    this.onError = function (err) {
+        console.error('Error: ' + JSON.stringify(err, null, 4));
+    };
+
+    this.onProgress = function (progress) {
+        //console.log('Progress: ' + (progress.loaded/progress.total*100) + '%');
+    };
+
+    this.onRenderPage = function (page) {
+        console.log('Page: ' + page);
+    };
+
+    scope.options = {
+        pdfUrl: 'pdf/pdf.pdf',
+        onLoad: tCtrl.onLoad,
+        onProgress: tCtrl.onProgress,
+        onError: tCtrl.onError,
+        onRenderPage: tCtrl.onRenderPage
+    };
+    console.log(JSON.stringify(scope.options, null, 4));
+});
